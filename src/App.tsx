c8fb379d74a1dcc111bc308fd0e6ea96625e1b97
shubhamElemensis <168633512+shubhamElemensis@users.noreply.github.com>
2024-05-21 10:59:28 +0000

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import React from "react";
-interface book {
+import React, { useState } from "react";
+// interface book {
+//   name: string;
+//   age: number;
+// }
+
+interface user {
   name: string;
   age: number;
 }
-export default function App({ name, age }: book) {
+export default function App() {
+  const [input, setinput] = useState<user| null >({ name: "", age: 0 });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+
+      setinput({
+        name: e.target.value,
+        age: e.target.value
+    });
+  };
   return (
     <div>
-      {name}
-      {age}
+      <input
+        type="text"
+        name="name"
+        onChange={handleChange}
+        placeholder="name"
+      />
+      <input
+        type="number"
+        name="age"
+        onChange={handleChange}
+        placeholder="age"
+      />
     </div>
   );
 }
